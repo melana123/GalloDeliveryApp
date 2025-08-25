1ab430b749b517bfa6aedf5de9dd3001e1753b6c
melana123 <melana.etec@gmail.com>
2025-08-25 07:56:17 -0300

    Criando o colors ts

---
+++ galloDelivery/constants/Colors.ts
@@ -0,0 +1,29 @@
+import { Background } from "@react-navigation/elements";
+
+const tintColorLight ='#2f95dc';
+const tintColorDark ='#ffffff';
+
+export default {
+    light: {
+        text: '#000000',
+        background:'#ffffff',
+        tint: tintColorLight,
+        tabIconDefault:'#cccccc',
+        tabInconSelected: tintColorLight
+
+    },
+    dark: {
+        text:'#ffffff',
+        Background:'#000000',
+        tint: tintColorLight,
+        tabIconDefault:'#cccccc',
+        tabIconSelected: tintColorLight
+
+    },
+    primary: '#20E1B2',
+    lightGrey:'#FCF8FF',
+    grey:'#EEE9F0',
+    medium:'#9F9AA1',
+    mediumDark:'#424242',
+    green: '#437919'
+}
